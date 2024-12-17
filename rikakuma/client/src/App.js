1377bf196de41033b9965f8fc7564af68393ee8d
ralphdaryn <ralphdarync@gmail.com>
2024-12-17 15:34:31 -0500

    update footer

--- rikakuma/client/src/App.js
+++ rikakuma/client/src/App.js
@@ -1,15 +1,15 @@
-// import logo from './logo.svg';
 import "./App.scss";
 import Header from "../src/components/Header/Header";
-// import Footer from "../src/components/Footer/Footer";
+import Footer from "../src/components/Footer/Footer";
 
 const App = () => {
   return (
     <div className="app">
-      <div>
-        <Header />
-        {/* <Footer /> */}
+      <Header />
+      <div className="background">
+        
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ rikakuma/client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <p className="footer__title">Thank you for visiting!</p>
+      </div>
+    </div>
+  );
+};
+export default Footer;
