7536774ca3dac442fc9cb684877c418ca89a919c
ralphdaryn <ralphdarync@gmail.com>
2024-12-19 13:54:48 -0500

    update image sticker container

--- rikakuma/client/src/components/Card/Card.js
+++ rikakuma/client/src/components/Card/Card.js
@@ -1,4 +1,5 @@
 import "./Card.scss";
+import stickerImg from "../../assets/images/sticker.png";
 
 const Card = () => {
   return (
@@ -7,6 +8,12 @@ const Card = () => {
         <h3 className="card__title">Welcome!</h3>
         <h3 className="card__subtitle">Thank you for visiting!</h3>
         <div className="card__button">Shop All</div>
+      </div>
+      <div className="card__container">
+        <img className="card__image" src={stickerImg} alt="sticker pic"></img>
+        <h3 className="card__title">Featured:</h3>
+        <h3 className="card__subtitle">Stickers!</h3>
+        <div className="card__button">Shop</div>
       </div>
     </div>
   );
