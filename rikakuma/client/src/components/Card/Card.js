354df7dd00764a1a2e254d5213a8f2bbc2e705c8
ralphdaryn <ralphdarync@gmail.com>
2025-01-14 00:07:56 -0500

    update card container, image and font

--- rikakuma/client/src/components/Card/Card.js
+++ rikakuma/client/src/components/Card/Card.js
@@ -1,19 +1,44 @@
 import "./Card.scss";
 import stickerImg from "../../assets/images/sticker.png";
+import keychainImg from "../../assets/images/keychain.png";
 
 const Card = () => {
   return (
     <div className="card">
       <div className="card__container">
-        <h3 className="card__title">Welcome!</h3>
-        <h3 className="card__subtitle">Thank you for visiting!</h3>
+        <div className="card__wrapper">
+          <h4 className="card__title">Welcome!</h4>
+          <p className="card__subtitle">Thank you for visiting!</p>
+        </div>
         <div className="card__button">Shop All</div>
       </div>
       <div className="card__container">
-        <img className="card__image" src={stickerImg} alt="sticker pic"></img>
-        <h3 className="card__title">Featured:</h3>
-        <h3 className="card__subtitle">Stickers!</h3>
-        <div className="card__button">Shop</div>
+        <div className="card__wrapper">
+          <div className="card__image-container">
+            <img
+              className="card__image"
+              src={stickerImg}
+              alt="sticker pic"
+            ></img>
+          </div>
+          <h3 className="card__title">Featured Items:</h3>
+          <p className="card__title">Stickers!</p>
+          <div className="card__button">Shop</div>
+        </div>
+      </div>
+      <div className="card__container">
+        <div className="card__wrapper">
+          <h3 className="card__title">Featured Items:</h3>
+          <p className="card__title">Key Chains!</p>
+          <div className="card__button">Shop</div>
+          <div className="card__image-container">
+            <img
+              className="card__image"
+              src={keychainImg}
+              alt="sticker pic"
+            ></img>
+          </div>
+        </div>
       </div>
     </div>
   );
