542bb37b1b87e03489edaa6555ccf69e90a7a20a
ralphdaryn <ralphdarync@gmail.com>
2025-01-15 11:52:49 -0500

    style tablet layout card

--- rikakuma/client/src/components/Card/Card.js
+++ rikakuma/client/src/components/Card/Card.js
@@ -12,31 +12,34 @@ const Card = () => {
         </div>
         <div className="card__button">Shop All</div>
       </div>
-      <div className="card__container">
-        <div className="card__wrapper">
-          <div className="card__image-container">
-            <img
-              className="card__image"
-              src={stickerImg}
-              alt="sticker pic"
-            ></img>
+      <div className="card__container-feature">
+        <div className="card__container">
+          <div className="card__wrapper">
+            <div className="card__image-container">
+              <img
+                className="card__image"
+                src={stickerImg}
+                alt="sticker pic"
+              ></img>
+            </div>
+            <h3 className="card__title">Featured Items:</h3>
+            <p className="card__subtitle">Stickers!</p>
+            <div className="card__button">Shop</div>
           </div>
-          <h3 className="card__title">Featured Items:</h3>
-          <p className="card__subtitle">Stickers!</p>
-          <div className="card__button">Shop</div>
         </div>
-      </div>
-      <div className="card__container">
-        <div className="card__wrapper">
-          <h3 className="card__title">Featured Items:</h3>
-          <p className="card__subtitle">Key Chains!</p>
-          <div className="card__button">Shop</div>
-          <div className="card__image-container">
-            <img
-              className="card__image"
-              src={keychainImg}
-              alt="keychain pic"
-            ></img>
+
+        <div className="card__container">
+          <div className="card__wrapper">
+            <h3 className="card__title">Featured Items:</h3>
+            <p className="card__subtitle">Key Chains!</p>
+            <div className="card__button">Shop</div>
+            <div className="card__image-container">
+              <img
+                className="card__image"
+                src={keychainImg}
+                alt="keychain pic"
+              ></img>
+            </div>
           </div>
         </div>
       </div>
