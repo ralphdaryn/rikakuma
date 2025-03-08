371bae250dd9c0484eacf86345e4954989c7b496
ralphdaryn <ralphdarync@gmail.com>
2025-03-08 17:19:33 -0500

    style sticker and adjust layout for mobile, tablet

--- client/src/components/Stickers/Stickers.js
+++ client/src/components/Stickers/Stickers.js
@@ -1,25 +1,59 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 import "./Stickers.scss";
-import Sticker from "../../assets/images/in_out_buddy.jpg";
+import Sticker1 from "../../assets/images/in_out_buddy.jpg";
+import Sticker2 from "../../assets/images/luna_buddy.jpg";
+import Sticker3 from "../../assets/images/dan_the_penguin.jpg";
+
+const stickersData = [
+  { id: 1, image: Sticker1, name: "In and Out Buddies", price: "$2.00" },
+  { id: 2, image: Sticker2, name: "Luna Buddy", price: "$2.00" },
+  { id: 3, image: Sticker3, name: "Dan the Penguin", price: "$2.00" },
+];
 
 const Stickers = () => {
   return (
     <div className="stickers">
       <h2 className="stickers__title">Stickers</h2>
-      <div className="stickers__container">
-        <div className="stickers__wrapper">
-          <img className="stickers__image" src={Sticker} alt="Charms" />
-        </div>
-        <div className="stickers__subtitle">
-          <div className="stickers__subtitle-container">
-            <h3 className="stickers__text">In and Out Buddies</h3>
-            <p className="stickers__subtext">From $3.00</p>
-          </div>
-        </div>
-        <div className="stickers__button">
-          <div className="stickers__button-text">Add to Cart</div>
-        </div>
-      </div>
+
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1.3}
+        pagination={{ clickable: true }}
+        centeredSlides={true}
+        breakpoints={{
+          768: {
+            slidesPerView: 3,
+          },
+        }}
+        className="stickers__swiper"
+      >
+        {stickersData.map((sticker) => (
+          <SwiperSlide key={sticker.id} className="stickers__slide">
+            <div className="stickers__container">
+              <div className="stickers__wrapper">
+                <img
+                  className="stickers__image"
+                  src={sticker.image}
+                  alt={sticker.name}
+                />
+              </div>
+              <div className="stickers__subtitle">
+                <div className="stickers__subtitle-container">
+                  <h3 className="stickers__text">{sticker.name}</h3>
+                  <p className="stickers__subtext">From {sticker.price}</p>
+                </div>
+              </div>
+              <div className="stickers__button">
+                <div className="stickers__button-text">Add to Cart</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
+
 export default Stickers;
