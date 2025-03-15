a3f38f4e9759c0a61b1a4cc039b11bf9d1ecde48
ralphdaryn <ralphdarync@gmail.com>
2025-03-15 01:24:05 -0400

    add charms images and style layout

--- client/src/components/Charm/Charms.js
+++ client/src/components/Charm/Charms.js
@@ -2,24 +2,50 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { useLocation } from "react-router-dom";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./Charms.scss";
+
+// Importing charm images
 import Charms1 from "../../assets/images/kuromi.jpg";
 import Charms2 from "../../assets/images/my_melody.jpg";
-import Charms4 from "../../assets/images/cinnamoroll.jpg";
 import Charms3 from "../../assets/images/miffy.jpg";
+import Charms4 from "../../assets/images/cinnamoroll.jpg";
+import Charms5 from "../../assets/images/1_charm-iron.jpg";
+import Charms6 from "../../assets/images/2_charm-bronze.jpg";
+import Charms7 from "../../assets/images/3_charm-silver.jpg";
+import Charms8 from "../../assets/images/4_charm-gold.jpg";
+import Charms9 from "../../assets/images/5_charm-platinum.jpg";
+import Charms10 from "../../assets/images/6_charm-diamond.jpg";
+import Charms11 from "../../assets/images/7_charm-ascendant.jpg";
+import Charms12 from "../../assets/images/8_charm-immortal.jpg";
+import Charms13 from "../../assets/images/9_charm-radiant.jpg";
+import Charms14 from "../../assets/images/10_charm-riot.jpg";
 
-const charmsData = [
+// Charm data for first row
+const firstRowCharms = [
   { id: 1, image: Charms1, name: "Kuromi x Sonny Angel", price: "$10.00" },
   { id: 2, image: Charms2, name: "My Melody x Sonny Angel", price: "$10.00" },
   { id: 3, image: Charms3, name: "Miffy x Sonny Angel", price: "$10.00" },
   { id: 4, image: Charms4, name: "Cinnamoroll x Sonny Angel", price: "$10.00" },
 ];
 
+// Charm data for second row
+const secondRowCharms = [
+  { id: 5, image: Charms5, name: "Iron Charm", price: "$10.00" },
+  { id: 6, image: Charms6, name: "Bronze Charm", price: "$10.00" },
+  { id: 7, image: Charms7, name: "Silver Charm", price: "$10.00" },
+  { id: 8, image: Charms8, name: "Gold Charm", price: "$10.00" },
+  { id: 9, image: Charms9, name: "Platinum Charm", price: "$10.00" },
+  { id: 10, image: Charms10, name: "Diamond Charm", price: "$10.00" },
+  { id: 11, image: Charms11, name: "Ascendant Charm", price: "$10.00" },
+  { id: 12, image: Charms12, name: "Immortal Charm", price: "$10.00" },
+  { id: 13, image: Charms13, name: "Radiant Charm", price: "$10.00" },
+  { id: 14, image: Charms14, name: "Riot Charm", price: "$10.00" },
+];
+
 const Charms = () => {
   const location = useLocation();
-  const isStandalonePage = location.pathname === "/charms"; // Check if we are on /charms page
+  const isStandalonePage = location.pathname === "/charms";
 
   return (
     <div
@@ -29,6 +55,7 @@ const Charms = () => {
     >
       <h2 className="charms__title">Charms (Keychain)</h2>
 
+      {/* First Row */}
       <Swiper
         spaceBetween={0}
         slidesPerView={1.3}
@@ -37,13 +64,11 @@ const Charms = () => {
         modules={[Navigation]}
         centeredSlides={true}
         breakpoints={{
-          768: {
-            slidesPerView: 3,
-          },
+          768: { slidesPerView: 3 },
         }}
         className="charms__swiper"
       >
-        {charmsData.map((charm) => (
+        {firstRowCharms.map((charm) => (
           <SwiperSlide key={charm.id} className="charms__slide">
             <div className="charms__container">
               <div className="charms__wrapper">
@@ -54,10 +79,43 @@ const Charms = () => {
                 />
               </div>
               <div className="charms__subtitle">
-                <div className="charms__subtitle-container">
-                  <h3 className="charms__text">{charm.name}</h3>
-                  <p className="charms__subtext">From {charm.price}</p>
-                </div>
+                <h3 className="charms__text">{charm.name}</h3>
+                <p className="charms__subtext">From {charm.price}</p>
+              </div>
+              <div className="charms__button">
+                <div className="charms__button-text">Add to Cart</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Second Row */}
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1.3}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Navigation]}
+        centeredSlides={true}
+        breakpoints={{
+          768: { slidesPerView: 3 },
+        }}
+        className="charms__swiper"
+      >
+        {secondRowCharms.map((charm) => (
+          <SwiperSlide key={charm.id} className="charms__slide">
+            <div className="charms__container">
+              <div className="charms__wrapper">
+                <img
+                  className="charms__image"
+                  src={charm.image}
+                  alt={charm.name}
+                />
+              </div>
+              <div className="charms__subtitle">
+                <h3 className="charms__text">{charm.name}</h3>
+                <p className="charms__subtext">From {charm.price}</p>
               </div>
               <div className="charms__button">
                 <div className="charms__button-text">Add to Cart</div>
--- client/src/components/Stickers/Stickers.js
+++ client/src/components/Stickers/Stickers.js
@@ -2,9 +2,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { useLocation } from "react-router-dom";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./Stickers.scss";
+
+// Importing sticker images
 import Sticker1 from "../../assets/images/in_out_buddy.jpg";
 import Sticker2 from "../../assets/images/luna_buddy.jpg";
 import Sticker3 from "../../assets/images/dan_the_penguin.jpg";
@@ -13,8 +14,20 @@ import Sticker5 from "../../assets/images/brimsicle_buddy.jpg";
 import Sticker6 from "../../assets/images/jett_knife_buddy.jpg";
 import Sticker7 from "../../assets/images/tanghulu_buddy.jpg";
 import Sticker8 from "../../assets/images/pizza_buddy.jpg";
+import Sticker9 from "../../assets/images/1iron.jpg";
+import Sticker10 from "../../assets/images/2bronze.jpg";
+import Sticker11 from "../../assets/images/3silver.jpg";
+import Sticker12 from "../../assets/images/4gold.jpg";
+import Sticker13 from "../../assets/images/5platinum.jpg";
+import Sticker14 from "../../assets/images/6diamond.jpg";
+import Sticker15 from "../../assets/images/7ascendant.jpg";
+import Sticker16 from "../../assets/images/8immortal.jpg";
+import Sticker17 from "../../assets/images/9radiant.jpg";
+import Sticker18 from "../../assets/images/10riotbuddy.jpg";
+import Sticker19 from "../../assets/images/ALL_STICKERS.jpg";
 
-const stickersData = [
+// Sticker data for first row
+const firstRowStickers = [
   { id: 1, image: Sticker1, name: "In and Out Buddies", price: "$2.00" },
   { id: 2, image: Sticker2, name: "Luna Buddy", price: "$2.00" },
   { id: 3, image: Sticker3, name: "Dan the Penguin", price: "$2.00" },
@@ -25,9 +38,25 @@ const stickersData = [
   { id: 8, image: Sticker8, name: "Pizza Buddy", price: "$2.00" },
 ];
 
+// Sticker data for second row
+const secondRowStickers = [
+  { id: 9, image: Sticker19, name: "All Stickers", price: "$2.00" },
+  { id: 10, image: Sticker18, name: "Riot Buddy", price: "$2.00" },
+  { id: 11, image: Sticker9, name: "Iron", price: "$2.00" },
+  { id: 12, image: Sticker10, name: "Bronze", price: "$2.00" },
+  { id: 13, image: Sticker11, name: "Silver", price: "$2.00" },
+  { id: 14, image: Sticker12, name: "Gold", price: "$2.00" },
+  { id: 15, image: Sticker13, name: "Platinum", price: "$2.00" },
+  { id: 16, image: Sticker14, name: "Diamond", price: "$2.00" },
+  { id: 17, image: Sticker15, name: "Ascendant", price: "$2.00" },
+  { id: 18, image: Sticker16, name: "Immortal", price: "$2.00" },
+  { id: 19, image: Sticker17, name: "Radiant", price: "$2.00" },
+];
+
 const Stickers = () => {
   const location = useLocation();
   const isStandalonePage = location.pathname === "/stickers";
+
   return (
     <div
       className={`stickers ${
@@ -36,6 +65,7 @@ const Stickers = () => {
     >
       <h2 className="stickers__title">Stickers</h2>
 
+      {/* First Row */}
       <Swiper
         spaceBetween={0}
         slidesPerView={1.3}
@@ -44,13 +74,11 @@ const Stickers = () => {
         modules={[Navigation]}
         centeredSlides={true}
         breakpoints={{
-          768: {
-            slidesPerView: 3,
-          },
+          768: { slidesPerView: 3 },
         }}
         className="stickers__swiper"
       >
-        {stickersData.map((sticker) => (
+        {firstRowStickers.map((sticker) => (
           <SwiperSlide key={sticker.id} className="stickers__slide">
             <div className="stickers__container">
               <div className="stickers__wrapper">
@@ -61,10 +89,43 @@ const Stickers = () => {
                 />
               </div>
               <div className="stickers__subtitle">
-                <div className="stickers__subtitle-container">
-                  <h3 className="stickers__text">{sticker.name}</h3>
-                  <p className="stickers__subtext">From {sticker.price}</p>
-                </div>
+                <h3 className="stickers__text">{sticker.name}</h3>
+                <p className="stickers__subtext">From {sticker.price}</p>
+              </div>
+              <div className="stickers__button">
+                <div className="stickers__button-text">Add to Cart</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Second Row */}
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1.3}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Navigation]}
+        centeredSlides={true}
+        breakpoints={{
+          768: { slidesPerView: 3 },
+        }}
+        className="stickers__swiper"
+      >
+        {secondRowStickers.map((sticker) => (
+          <SwiperSlide key={sticker.id} className="stickers__slide">
+            <div className="stickers__container">
+              <div className="stickers__wrapper">
+                <img
+                  className="stickers__image"
+                  src={sticker.image}
+                  alt={sticker.name}
+                />
+              </div>
+              <div className="stickers__subtitle">
+                <h3 className="stickers__text">{sticker.name}</h3>
+                <p className="stickers__subtext">From {sticker.price}</p>
               </div>
               <div className="stickers__button">
                 <div className="stickers__button-text">Add to Cart</div>
