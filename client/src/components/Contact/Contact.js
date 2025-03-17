5d40eb94e53a3450a2ca6aa360d54a96675da845
ralphdaryn <ralphdarync@gmail.com>
2025-03-16 22:16:51 -0400

    update contact server

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,17 +1,62 @@
+import { useState } from "react";
 import "./Contact.scss";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    subject: "",
+    message: "",
+  });
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent default form submission
+    setStatus("Sending...");
+    try {
+      const response = await fetch(
+        "https://rikakuma.ca/.netlify/functions/contact",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        setStatus("Message sent successfully!");
+        setFormData({
+          name: "",
+          email: "",
+          phoneNumber: "",
+          subject: "",
+          message: "",
+        });
+      } else {
+        setStatus("Failed to send message. Please try again.");
+      }
+    } catch (error) {
+      setStatus("An error occurred. Please try again later.");
+    }
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="contact__container">
         <div className="contact__intro">
           <h1 className="contact__heading">Contact Us</h1>
-          <div className="contact__info"></div>
           <p className="contact__description">
-            Have questions?<br></br> We're here to help!
+            Have questions? We're here to help!
           </p>
         </div>
-        <form className="contact__form">
+        <form className="contact__form" onSubmit={handleSubmit}>
           <label htmlFor="name" className="contact__label">
             Name:
           </label>
@@ -21,6 +66,8 @@ const Contact = () => {
             name="name"
             className="contact__input"
             required
+            value={formData.name}
+            onChange={handleChange}
           />
 
           <label htmlFor="email" className="contact__label">
@@ -32,7 +79,35 @@ const Contact = () => {
             name="email"
             className="contact__input"
             required
+            value={formData.email}
+            onChange={handleChange}
           />
+
+          <label htmlFor="phoneNumber" className="contact__label">
+            Phone Number:
+          </label>
+          <input
+            type="text"
+            id="phoneNumber"
+            name="phoneNumber"
+            className="contact__input"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="subject" className="contact__label">
+            Subject:
+          </label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="contact__input"
+            required
+            value={formData.subject}
+            onChange={handleChange}
+          />
+
           <label htmlFor="message" className="contact__label">
             Message:
           </label>
@@ -41,12 +116,15 @@ const Contact = () => {
             name="message"
             className="contact__textarea"
             required
+            value={formData.message}
+            onChange={handleChange}
           ></textarea>
 
           <button type="submit" className="contact__button">
             Submit
           </button>
         </form>
+        {status && <p className="contact__status">{status}</p>}
       </div>
     </div>
   );
